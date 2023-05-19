2c7f18adf44a8a314ef94a67f4a4099d2e86b9d2
StreamTrooper34 <55407315+StreamTrooper34@users.noreply.github.com>
2023-05-19 23:37:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-// Fonction pour ajouter un nom et un point
-function ajouterNom() {
-  var nameInput = document.getElementById('nameInput');
-  var name = nameInput.value;
+document.addEventListener('DOMContentLoaded', function() {
+  var ajouterForm = document.getElementById('ajouter-form');
+  ajouterForm.addEventListener('submit', ajouterNom);
+  
+  afficherNoms();
+});
 
-  if (name) {
-    var existingNames = JSON.parse(localStorage.getItem('names')) || [];
-    existingNames.push({ name: name, points: 0 });
-    localStorage.setItem('names', JSON.stringify(existingNames));
-
-    nameInput.value = '';
+function ajouterNom(e) {
+  e.preventDefault();
+  
+  var nomInput = document.getElementById('nom-input');
+  var nom = nomInput.value.trim();
+  
+  if (nom !== '') {
+    var existingNoms = JSON.parse(localStorage.getItem('noms')) || [];
+    existingNoms.push({ nom: nom, points: 0 });
+    localStorage.setItem('noms', JSON.stringify(existingNoms));
+  
+    nomInput.value = '';
     afficherNoms();
   }
 }
 
-// Fonction pour récupérer les noms et les points du localStorage et les afficher
 function afficherNoms() {
-  var namesList = document.getElementById('namesList');
-  namesList.innerHTML = '';
+  var nomsListe = document.getElementById('noms-liste');
+  nomsListe.innerHTML = '';
 
-  var existingNames = JSON.parse(localStorage.getItem('names')) || [];
-  existingNames.forEach(function (item) {
-    var listItem = document.createElement('div');
-    listItem.classList.add('name');
+  var existingNoms = JSON.parse(localStorage.getItem('noms')) || [];
 
-    var nameSpan = document.createElement('span');
-    nameSpan.textContent = item.name;
-    listItem.appendChild(nameSpan);
+  existingNoms.forEach(function(nomObj) {
+    var nomItem = document.createElement('div');
+    nomItem.classList.add('nom-item');
+
+    var nomSpan = document.createElement('span');
+    nomSpan.classList.add('nom');
+    nomSpan.textContent = nomObj.nom;
 
     var pointsSpan = document.createElement('span');
-    pointsSpan.textContent = item.points;
-    listItem.appendChild(pointsSpan);
+    pointsSpan.classList.add('points');
+    pointsSpan.textContent = nomObj.points;
 
-    var addButton = document.createElement('button');
-    addButton.textContent = '+1';
-    addButton.addEventListener('click', function () {
-      item.points++;
-      afficherNoms();
+    var incrementBtn = document.createElement('button');
+    incrementBtn.classList.add('btn');
+    incrementBtn.textContent = '+1';
+    incrementBtn.addEventListener('click', function() {
+      incrementerPoints(nomObj);
     });
-    listItem.appendChild(addButton);
 
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Supprimer';
-    deleteButton.addEventListener('click', function () {
-      existingNames = existingNames.filter(function (i) {
-        return i.name !== item.name;
-      });
-      localStorage.setItem('names', JSON.stringify(existingNames));
-      afficherNoms();
+    var supprimerBtn = document.createElement('button');
+    supprimerBtn.classList.add('btn');
+    supprimerBtn.textContent = 'Supprimer';
+    supprimerBtn.addEventListener('click', function() {
+      supprimerNom(nomObj);
     });
-    listItem.appendChild(deleteButton);
 
-    namesList.appendChild(listItem);
+    nomItem.appendChild(nomSpan);
+    nomItem.appendChild(pointsSpan);
+    nomItem.appendChild(incrementBtn);
+    nomItem.appendChild(supprimerBtn);
+
+    nomsListe.appendChild(nomItem);
   });
 }
 
-// Charger les noms et les points lors du chargement de la page
-window.addEventListener('load', function () {
+function incrementerPoints(nomObj) {
+  nomObj.points++;
+  var existingNoms = JSON.parse(localStorage.getItem('noms')) || [];
+  localStorage.setItem('noms', JSON.stringify(existingNoms));
   afficherNoms();
-});
+}
 
-// Sauvegarder les données dans le localStorage avant de quitter la page
-window.addEventListener('beforeunload', function (event) {
-  var existingNames = JSON.parse(localStorage.getItem('names')) || [];
-  localStorage.setItem('names', JSON.stringify(existingNames));
-});
+function supprimerNom(nomObj) {
+  var existingNoms = JSON.parse(localStorage.getItem('noms')) || [];
+  var index = existingNoms.findIndex(function(nom) {
+    return nom.nom === nomObj.nom;
+  });
+
+  if (index !== -1) {
+    existingNoms.splice(index, 1);
+    localStorage.setItem('noms', JSON.stringify(existingNoms));
+    afficherNoms();
+  }
+}
